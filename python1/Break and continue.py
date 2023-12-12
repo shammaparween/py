3fff8086fdfc158a8dbf1104735dd3a1023cea2e
shammaparween <153449214+shammaparween@users.noreply.github.com>
2023-12-12 15:31:22 +0530

    Add files via upload

---
+++ python1/Break and continue.py
@@ -0,0 +1,8 @@
+# break means seedha loop chodkar nikal jao
+# continue means 1 iteration ko skip kar do or baki ka continue kro
+#  Ex of CONTINUE next continue.py me hain
+for i in range(7,15):
+    if(i==13):
+        break
+    print("5 X", (i+1),  "=" , 5*(i+1))
+    
